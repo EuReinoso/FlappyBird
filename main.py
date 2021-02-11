2e1ea755fd26e3f7865cee7667c28f13605e787b
Lucas Reinoso <lucas.reinoso.br@gmail.com>
2021-02-11 09:41:22 -0300

    Pipes and Pipe_Collide

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, sys,random
 
 pygame.init()
 
+
 WINDOW_SIZE = (640, 480)
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Flappy Bird")
@@ -13,15 +14,17 @@ loop = True
 time = pygame.time.Clock()
 
 ground = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0','0','0','0']
-pipes = ['0', '0', '0', '0', '0','0', '0', '0', '0', '0']
+pipe_rect_bottom = []
+pipe_rect_top = []
+pipe_vel = 0.5
+
+def gen_pipes(y_pos):
+    pipe_rect_bottom.append(pygame.Rect(DISPLAY_SIZE[0],90 - 64 + y_pos,32,64))
+    pipe_rect_top.append(pygame.Rect(DISPLAY_SIZE[0],90 - 64 -64 - 35 + y_pos,32,64))
 
 def random_num(a,b):
     x = random.randint(a,b)
     return x
-
-random_x_pipe = random_num(2,50)
-random_y_pipe = random_num(2,50)
-
 
 bird_img = pygame.image.load("assets/bird.png")
 grass_img = pygame.image.load("assets/grass.png")
@@ -35,14 +38,16 @@ bird_rect = pygame.Rect(30, 30, bird_img.get_width(), bird_img.get_height())
 y_momentum = 0
 Y_VEL = 0.2
 
-JUMP_FORCE = 4
+JUMP_FORCE = 3.5
 
 alive = True
+start  = True
 
 ticks = 0
 
 scroll = 0
 scroll_vel = 0.5
+
 while loop:
 
     display.fill((135, 206, 235))
@@ -50,40 +55,65 @@ while loop:
     #scrolling
     if alive:
         scroll += scroll_vel
-        if scroll >= 120:
-            scroll = 0
-            random_x_pipe = random_num(2,10)
-            random_y_pipe = random_num(2,10)
 
+        if scroll >= 120:
+            scroll = 0          
 
     #tiles
     tile_rect = []
-    x = 0
+    x_1 = 0
     for tile in ground:
         if tile == '0':
-            display.blit(grass_img, (x * TILE_SIZE - scroll, 90 - 8))
-            display.blit(grass_img, (x * TILE_SIZE - scroll + DISPLAY_SIZE[0], 90 - 8))
-            tile_rect.append(pygame.Rect(x * TILE_SIZE - scroll, 90 - 8, TILE_SIZE, TILE_SIZE))
-            tile_rect.append(pygame.Rect(x * TILE_SIZE - scroll + DISPLAY_SIZE[0],90-8,TILE_SIZE,TILE_SIZE))
-        x += 1
+            display.blit(grass_img, (x_1 * TILE_SIZE - scroll, 90 - 8))
+            display.blit(grass_img, (x_1 * TILE_SIZE - scroll + DISPLAY_SIZE[0], 90 - 8))
+            tile_rect.append(pygame.Rect(x_1 * TILE_SIZE - scroll, 90 - 8, TILE_SIZE, TILE_SIZE))
+            tile_rect.append(pygame.Rect(x_1 * TILE_SIZE - scroll + DISPLAY_SIZE[0],90-8,TILE_SIZE,TILE_SIZE))
+        x_1 += 1
 
     #pipes
+    ticks += 1
+    if ticks == 60:
+        y_pos =random_num(10,40)
+        gen_pipes(y_pos)
+        ticks = 0
 
-    if ticks == random_num(1,60):
-        display.blit(pipe_img,(random_num(1,120) * 16 - scroll + DISPLAY_SIZE[0],90-64 + random_y_pipe))
+    for pipe in pipe_rect_bottom:
+        pipe.x -= pipe_vel
+        if pipe.x <= 0:
+            pipe_rect_bottom.pop(0)
+    for pipe in pipe_rect_top: 
+        pipe.x -= pipe_vel
+        if pipe.x <= 0:
+            pipe_rect_top.pop(0)
 
+    for pipe in pipe_rect_bottom:
+        display.blit(pipe_img,(pipe.x,pipe.y))
+    for pipe in pipe_rect_top:
+        display.blit(pygame.transform.flip(pipe_img,True,True),(pipe.x,pipe.y))
+
+    #pipe collision
+    for pipe in pipe_rect_bottom:
+        if bird_rect.colliderect(pipe):
+            alive = False
+            pipe_vel = 0
+    for pipe in pipe_rect_top:
+        if bird_rect.colliderect(pipe):
+            alive = False
+            pipe_vel = 0        
 
     #ground collision
     for tile in tile_rect:
         if bird_rect.colliderect(tile):
             bird_rect.bottom = tile.top
             alive = False
+            game_over = 0
 
     #gravity
     bird_rect.y += y_momentum
     y_momentum += Y_VEL
     if y_momentum >= 2:
         y_momentum = 2
+
 
     #events
     for event in pygame.event.get():
@@ -95,10 +125,6 @@ while loop:
             if event.key == pygame.K_SPACE: #jump
                 if alive:
                     y_momentum -= JUMP_FORCE
-    #timer
-    ticks += 1
-    if ticks >= 60:
-        ticks = 0
 
     display.blit(bird_img, (bird_rect.x, bird_rect.y))
     window.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
